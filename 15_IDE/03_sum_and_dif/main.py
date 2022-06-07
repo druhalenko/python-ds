e0fdbdb9d17335bdacd1c7ad270d369abac0d346
Dmitriy <druhalenko@gmail.com>
2022-06-07 18:19:00 +0300

    Готово

--- 15_IDE/03_sum_and_dif/main.py
+++ 15_IDE/03_sum_and_dif/main.py
@@ -1,1 +1,27 @@
-# TODO здесь писать код
+def func_summa(N):
+ summa = 0
+ while N > 0:
+  num = N % 10
+  summa = summa + num
+  N = N // 10
+ print('Сумма чисел:', summa)
+ return summa
+
+def func_count(N):
+ count = 0
+ while N > 0:
+  count += 1
+  N //= 10
+ print('Количество цифр в числе:', count)
+ return count
+
+
+def res(summa, count):
+ res = summa - count
+ print('Разность суммы и количества цифр:', res)
+
+N = int(input('Введите число: '))
+
+summa = func_summa(N)
+count = func_count(N)
+res(summa, count)
