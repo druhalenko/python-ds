8b7b792263dbda6486be6f40da42ab542d1a78a0
Dmitriy <druhalenko@gmail.com>
2022-06-08 15:50:23 +0300

    Готово

--- 15_IDE/07_years/main.py
+++ 15_IDE/07_years/main.py
@@ -1,11 +1,11 @@
 def three_digits(y1, y2):
- for i in range(y1, y2 + 1):
-  a = i // 1000
-  b = i // 100 % 10
-  c = i // 10 % 10
-  d = i % 10
-  if a == b == c or b == c == d or c == d == a or a == b == d:
-   print(i)
+    for i in range(y1, y2 + 1):
+        a = i // 1000
+        b = i // 100 % 10
+        c = i // 10 % 10
+        d = i % 10
+        if a == b == c or b == c == d or c == d == a or a == b == d:
+            print(i)
 
 years = int(input('Введите первый год: '))
 years2 = int(input('Введите второй год: '))
