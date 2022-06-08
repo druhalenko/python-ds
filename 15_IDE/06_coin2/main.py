73833d86ea5b57918ab9a06116185261f05c90c6
Dmitriy <druhalenko@gmail.com>
2022-06-08 12:59:51 +0300

    Готово

--- 15_IDE/06_coin2/main.py
+++ 15_IDE/06_coin2/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+def find_monetka(c1, c2, r):
+ if c1 <= r and c2 <= r:
+  print('Монетка где-то рядом')
+ else:
+  print('Монетки рядом нет')
+
+print('Введите координаты монетки: ')
+x = float(input("Введите x: "))
+y = float(input("Введите y: "))
+r = int(input("Введите радиус : "))
+
+find_monetka(x, y, r)
