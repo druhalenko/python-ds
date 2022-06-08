9842d0fc3fff7e4dab6b4c4a00aed2468abf6a7f
Dmitriy <druhalenko@gmail.com>
2022-06-08 15:38:01 +0300

    Готово

--- 15_IDE/02_session/main.py
+++ 15_IDE/02_session/main.py
@@ -1,14 +1,21 @@
-print("Введите первую точку")
+print('Введите первую точку')
 x1 = float(input('X: '))
 y1 = float(input('Y: '))
-print("\nВведите вторую точку")
+print('\nВведите вторую точку')
 x2 = float(input('X: '))
 y2 = float(input('Y: '))
 
+
 x_diff = x1 - x2
 y_diff = y1 - y2
-k = x_diff / y_diff
+if x_diff == 0:
+    k = 1
+else:
+    k = y_diff / x_diff
 b = y2 - k * x2
 
-print("Уравнение прямой, проходящей через эти точки:")
-print("y = ", k, " * x + ", b)
+
+print('Уравнение прямой, проходящей через эти точки: ')
+print('y = ', k, ' * x + ', b)
+
+
