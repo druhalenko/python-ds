7f29eace5cf5fc3b2f80e24e00b8b034c9b0c964
Dmitriy <druhalenko@gmail.com>
2022-06-08 15:48:28 +0300

    Готово

--- 15_IDE/05_least_divisor/main.py
+++ 15_IDE/05_least_divisor/main.py
@@ -1,11 +1,11 @@
 def get_min_divider(a):
- min_divider = 1
- for i in range(1, a + 1):
-  if a % i == 0:
-   min_divider = i
-  if min_divider > 1:
-   break
- return min_divider
+    min_divider = 1
+    for i in range(1, a + 1):
+        if a % i == 0:
+            min_divider = i
+        if min_divider > 1:
+           break
+    return min_divider
 
 number = int(input('Введите число: '))
 print('Наименьший делитель, отличный от единицы:', get_min_divider(number))
